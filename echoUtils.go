6a2fbab22d557107d70f62767a9f0ed45d54eb3c
blagoySimandoff <baggy.simandoff@gmail.com>
2022-09-12 02:19:20 +0300

    fix

--- echoUtils.go
+++ echoUtils.go
@@ -13,54 +13,35 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/gommon/log"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func loadData(collection string, order string) (*ProductData, error) {
 	coll := mongoClient.Database("Products").Collection(collection)
-	var (
-		opt *options.FindOptions
-	)
-	collation := options.Collation{
-		Locale:          "en_US",
-		NumericOrdering: true,
-	}
-	if order == "LtoH" {
-		opt = options.Find().SetSort(bson.D{{Key: "price", Value: 1}}).SetCollation(&collation)
-		return findFiles(coll, opt)
-	} else if order == "HtoL" {
-		// Sort by `price` field descending
-		opt = options.Find().SetSort(bson.D{{Key: "price", Value: -1}}).SetCollation(&collation)
-		return findFiles(coll, opt)
-	}
 
-	return findFiles(coll, nil)
-}
-
-func findFiles(coll *mongo.Collection, opt *options.FindOptions) (*ProductData, error) {
-	var (
-		p    ProductData
-		docs *mongo.Cursor
-		err  error
-	)
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	if opt == nil {
-		docs, err = coll.Find(ctx, bson.D{})
-	} else {
-		docs, err = coll.Find(ctx, bson.D{}, opt)
+
+	opt := options.Find()
+	if order == "LtoH" {
+		opt.SetSort(bson.D{{Key: "price", Value: 1}})
+	} else if order == "HtoL" {
+		opt.SetSort(bson.D{{Key: "price", Value: -1}})
+	} else if order == "newest" || order == "" {
+		// we do nothing to Options
+		_ = ""
 	}
+	docs, err := coll.Find(ctx, bson.D{}, opt)
 	if err != nil {
-		log.Error("error finding the docs:", err)
 		return nil, err
 	}
+	var p ProductData
 	if err := docs.All(ctx, &p.Products); err != nil {
 		log.Error("error converting the cursor ro array:", err)
 		return nil, err
 	}
 	return &p, nil
+
 }
 
 type Template struct {
@@ -82,7 +63,12 @@ type Template struct {
 // }
 
 func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
+	err := t.templates.ExecuteTemplate(w, name, data)
+	if err != nil {
+		fmt.Println("Template error:", err)
+		return err
+	}
+	return nil
 }
 
 func PriceStringToInt(a string) (int, error) {
@@ -94,75 +80,27 @@ func PriceStringToInt(a string) (int, error) {
 	return aInt, nil
 }
 
-func MultiplyStringPrices(a, b string) (string, error) {
-	var (
-		err  error
-		aInt int
-		bInt int
-	)
-	aInt, err = PriceStringToInt(a)
-	if err != nil {
-		return "", err
-	}
-	bInt, err = PriceStringToInt(b)
-	if err != nil {
-		return "", err
-	}
-	product := aInt * bInt
-	productString := strconv.Itoa(product)
-	productString = AddDecimalPointToString(productString)
-	return productString, nil
-}
-
-func AddStringPrices(a, b string) (string, error) {
-	var (
-		err  error
-		aInt int
-		bInt int
-	)
-	aInt, err = PriceStringToInt(a)
-	if err != nil {
-		return "", err
-	}
-	bInt, err = PriceStringToInt(b)
-	if err != nil {
-		return "", err
-	}
-	result := aInt + bInt
-	resultString := strconv.Itoa(result)
-	resultString = AddDecimalPointToString(resultString)
-	return resultString, nil
-}
-func AddDecimalPointToString(a string) string {
-	index := len(a) - 2 // 000000.00 two places decimal
-	a = a[:index] + "." + a[index:]
-	return a
-}
 func loadCart(sess *sessions.Session) (*CartProducts, error) {
 	products, err := loadData("products", "")
 	if err != nil {
 		return nil, err
 	}
 	var cart CartProducts
-	cart.Total = "0"
+	cart.Total = 0
 	for key, count := range sess.Values {
 		for _, p := range products.Products {
 			if key == p.Id {
-				subtotal, err := MultiplyStringPrices(count.(string), p.Price)
-				fmt.Println(subtotal)
+				subtotal := count.(int) * p.Price
 				if err != nil {
 					return nil, err
 				}
 				x := CartProduct{
 					Pr:       p,
-					Count:    count.(string),
+					Count:    count.(int),
 					Subtotal: subtotal,
 				}
 				cart.Products = append(cart.Products, x)
-				cart.Total, err = AddStringPrices(subtotal, cart.Total)
-				if err != nil {
-					return nil, err
-				}
+				cart.Total += subtotal
 			}
 		}
 	}
